796dea967e87d43e6ad77d91cf24cad06f819903
vineetcoding <114997519+vineetcoding@users.noreply.github.com>
2023-03-04 19:19:56 +0530

    Calculator

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 import Calculator from './components/Calculator';
+import Heading from './components/Heading';
 
 function App() {
   return (
     <>
+      <Heading/>
       <Calculator/>
     </>
   );
---
+++ src/components/Heading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../components/Heading.css'
+
+export default function Heading() {
+  return (
+    <div>
+      <h1>Calculator Using React</h1>
+      
+    </div>
+  )
+}
